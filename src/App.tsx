19cfd77d74bdeaf18351a247e1e00bfd411bfda2
Western1833 <145915895+Western1833@users.noreply.github.com>
2025-06-15 19:03:49 +0300

    Update app to work with input.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,16 @@ import  '../src/App.css';
 
 import TodoList from './Todo-list.tsx';
 import Input from './Input.tsx';
+import { useState } from 'react';
 
 export default function TodoApp() {
+  const [todos, addTodos] = useState<string[]>([]);
+
+  const addTodosHandler = (newTodo: string) => {
+    if(newTodo.trim()){
+      addTodos(prev => [...prev, newTodo]);
+    }
+  }
   return (
     <div className="app-container">
       <div className="todo-card">
@@ -14,7 +22,7 @@ export default function TodoApp() {
         </div>
 
         {/* Input Section */}
-        <Input/>
+        <Input onAddTodo={addTodosHandler}/>
 
         {/* Todo List */}
         <TodoList/>
