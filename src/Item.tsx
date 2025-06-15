5f8452535b374da437d18f607e5b52865dfc4d16
Western1833 <145915895+Western1833@users.noreply.github.com>
2025-06-15 20:08:26 +0300

    Select test as completed works

--- src/Item.tsx
+++ src/Item.tsx
@@ -1,24 +1,25 @@
+import { useState } from "react";
+
 type ItemProp = {
     text: string;
 }
 
 export default function Item({ text }: ItemProp) {
+    const [checked, setChecked] = useState(false);
+
+    const toggleCheck = () =>  setChecked(prev => !prev);
+
     return (
-        <div className="todo-item">
-            <input type="checkbox" className="todo-checkbox" />
+        <div className={`todo-item ${checked ? 'completed' : ''}`}>
+            <input type="checkbox" className="todo-checkbox"
+            checked={checked}
+            onChange={toggleCheck}
+            />
             <span className="todo-text">{text}</span>
             <button className="delete-button">🗑️</button>
         </div>
     );
 }
-
-{/* <div className="todo-item">
-              <input type="checkbox" className="todo-checkbox" />
-              <span className="todo-text">Sample task</span>
-              <button className="delete-button">🗑️</button>
-            </div>
-            
-            {/* Completed Todo Item */}
 // <div className="todo-item completed">
 //   <input type="checkbox" className="todo-checkbox" checked />
 //   <span className="todo-text">Completed task</span>
